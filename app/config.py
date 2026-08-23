0f1659da4a51abd0e7ae8a54964d5c6ba3bd76d2
Антон Замотаев <toxik3772@gmail.com>
2026-08-23 17:19:02 +0300

    Fix

--- app/config.py
+++ app/config.py
@@ -1,12 +1,20 @@
 ﻿import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/events_db")
-    EVENTS_PROVIDER_URL: str = os.getenv("EVENTS_PROVIDER_URL", "http://events-provider.dev-2.python-labs.ru")
+    # ---- БД ----
+    # Если задана строка подключения целиком – используем её
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        f"postgresql+asyncpg://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DATABASE_NAME')}"
+    )
+    # ---- Events Provider ----
+    EVENTS_PROVIDER_URL: str = os.getenv(
+        "EVENTS_PROVIDER_URL",
+        "http://events-provider.dev-2.python-labs.ru"
+    )
     EVENTS_PROVIDER_API_KEY: str = os.getenv("EVENTS_PROVIDER_API_KEY", "")
     SYNC_INTERVAL_HOURS: int = int(os.getenv("SYNC_INTERVAL_HOURS", "24"))
 
